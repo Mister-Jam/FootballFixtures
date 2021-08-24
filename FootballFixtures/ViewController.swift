ed921cc2eb47ab678f3e5ef52d16dfa55d0c8965
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-24 19:23:28 +0100

    create project files

---
+++ FootballFixtures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootballFixtures
+//
+//  Created by King Bileygr on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
