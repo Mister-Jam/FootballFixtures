42d001870def6ae8ea778c8c5da4394d24da4af1
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-24 20:03:07 +0100

    create competitions vc

--- FootballFixtures/Controllers/CompetitionsViewController.swift
+++ FootballFixtures/Controllers/CompetitionsViewController.swift
@@ -11,7 +11,7 @@ class CompetitionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
     
