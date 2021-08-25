6d77c7d62fd5ad991b03dacbb16e4bdbce0736b3
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-25 01:23:48 +0100

    create competition custom cell

--- FootballFixtures/Views/CompetitionsTableViewCell.swift
+++ FootballFixtures/Views/CompetitionsTableViewCell.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class CompetitionsTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCellLayout()
+        accessoryType = .disclosureIndicator
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private let competitionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Kaneki Ken is the actor in charge"
+        return label
+    }()
+    
+    private func setupCellLayout() {
+        contentView.addSubview(competitionLabel)
+        competitionLabel.leadingAnchor(anchor: contentView.leadingAnchor, constant: 20)
+            .centerYAnchor(anchor: contentView.centerYAnchor)
+            .heightAnchorConstant(52)
+    }
+    
 }
