06abe31b3fac9a2f495f9c524140e5467538351d
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-25 01:21:59 +0100

    create model for todays fixtures cell

--- FootballFixtures/Models/TodaysFixtureModel.swift
+++ FootballFixtures/Models/TodaysFixtureModel.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct TodaysFixtureModel {
+    var startTime: String
+    var description: String
+    var firstTeam: String
+    var secondTeam: String
+    var firstTeamScore: String
+    var secondTeamScore: String
+    var minutes: String
+}
