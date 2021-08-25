fbc901914b75e589156367f686ec51dd47b46326
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-25 01:25:20 +0100

    setup todays fixture table controller

--- FootballFixtures/Controllers/TodaysFixturesViewController.swift
+++ FootballFixtures/Controllers/TodaysFixturesViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class TodaysFixturesViewController: UIViewController {
-
+class TodaysFixturesViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        navigationItem.title = "Today's Fixtures"
+        tableView.rowHeight = UITableView.automaticDimension
+        navigationController?.navigationBar.largeTitleTextAttributes = Constants.titleText
+        
+        self.setTableViewStyle(style: .grouped)
+            .setTableViewInsets()
+            
+        self.tableView.registerTableViewCell(cell: TodaysFixturesTableViewCell.self)
     }
-
-
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(with: TodaysFixturesTableViewCell.self, indexPath: indexPath)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        90
+    }
+    
 }
 
