b2b56690a7e1eb4aef565713771bea704828a695
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-29 18:34:47 +0100

    create class for network request

--- FootballFixtures/Networking/NetworkManager.swift
+++ FootballFixtures/Networking/NetworkManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+
+class NetworkManager: NetworkLoader {
+    
+    static let shared = NetworkManager()
+    
+    private init () { }
+    
+    public func loadRequest<T: Decodable>(path: String, model: T.Type, completion: @escaping ((Result<T, Error>)->Void)) {
+        
+        guard let url = URL(string: Constants.URLConstants.baseURL+path) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue(Constants.URLConstants.apiToken, forHTTPHeaderField: Constants.URLConstants.httpHeader)
+        URLSession.shared.decodeData(from: request, type: T.self) { result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            
+        }
+    }
+}
