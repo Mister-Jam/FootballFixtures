318c9aaa1f32165935aac2c608ac8232c1ab3606
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-29 18:32:32 +0100

    add network adapter

--- FootballFixtures/Networking/NetworkAdapters.swift
+++ FootballFixtures/Networking/NetworkAdapters.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct NetworkAdapter: NetworkLoader {
+
+    
+    var networkService: NetworkManager
+    
+    func loadRequest<T>(path: String, model: T.Type, completion: @escaping ((Result<T, Error>) -> Void)) where T : Decodable {
+        networkService.loadRequest(path: path, model: model) { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+    }
+    
+}
