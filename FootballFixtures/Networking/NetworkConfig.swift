cfac40e1bf4a7bd66f4e046f073bf8f02a6726f5
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-29 18:33:48 +0100

    setup network interface

--- FootballFixtures/Networking/NetworkConfig.swift
+++ FootballFixtures/Networking/NetworkConfig.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+
+protocol NetworkLoader {
+    func loadRequest<T: Decodable>(path: String, model: T.Type, completion: @escaping ((Result<T, Error>)->Void))
+}
+
+protocol ResultHandler {
+    associatedtype Model
+    func handleUrlRequest(result: Result<Model, Error>)
+}
