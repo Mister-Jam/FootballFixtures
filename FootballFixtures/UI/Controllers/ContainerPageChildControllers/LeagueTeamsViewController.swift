344ee12e4fafe760f37aba6ff20834325a60a958
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 11:32:00 +0100

    pass image url on tap

--- FootballFixtures/UI/Controllers/ContainerPageChildControllers/LeagueTeamsViewController.swift
+++ FootballFixtures/UI/Controllers/ContainerPageChildControllers/LeagueTeamsViewController.swift
@@ -7,45 +7,40 @@
 
 import UIKit
 
-class LeagueTeamsViewController: UICollectionViewController, ResultHandler {
+class LeagueTeamsViewController: UICollectionViewController {
 
-    var playersModel = [TeamSquadModel]()
-    var service: NetworkLoader?
+    var teamsData = [LeagueTeamsViewModel]()
     var presenter: Presenter?
         
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.registerCollectionViewCell(cell: LeagueTeamsCollectionViewCell.self)
         collectionView.backgroundColor = .white
-        
-    }
-    
-    internal func handleUrlRequest(result: Result<TeamSquadModel, Error>) {
-        switch result {
-        case .success(let data):
-            print(data)
-        case .failure(let error):
-            print(error)
-        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let data = LeagueTeamsViewModel.leagueTeamsData
+        teamsData = data.map { LeagueTeamsViewModel(dataSource: $0)}
         collectionView.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 5, right: 0)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        teamsData.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(with: LeagueTeamsCollectionViewCell.self, indexPath: indexPath)
-        cell.backgroundColor = .red
-        return cell
+        let item = teamsData[indexPath.row]
+        if let cell = collectionView.dequeueReusableCell(with: LeagueTeamsCollectionViewCell.self, indexPath: indexPath) as? LeagueTeamsCollectionViewCell {
+            cell.configure(with: item)
+            return cell
+        }
+        return UICollectionViewCell()
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        presenter?.presentController(title: "Manchester United", id: "66", item: self.navigationItem, controller: self)
+        let item = teamsData[indexPath.row]
+        presenter?.presentController(title: item.teamName, id: "\(item.teamID)", item: self.navigationItem, controller: self, url: item.teamLogoUrl)
         
     }
 }
