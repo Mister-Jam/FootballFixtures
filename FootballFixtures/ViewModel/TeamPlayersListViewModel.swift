8109d77d5e28747261305b3ebda77f519332832f
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 11:08:31 +0100

    create players list view model

--- FootballFixtures/ViewModel/TeamPlayersListViewModel.swift
+++ FootballFixtures/ViewModel/TeamPlayersListViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+struct TeamPlayersListViewModel {
+    var playerShirtNumber: String
+    var playerName: String
+    var playerPosition: String
+    var shirtNumber: Int?
+    
+    
+    init(dataSource: PlayerModel) {
+        if let shirtNumber = dataSource.shirtNumber,
+           let position = dataSource.position {
+            self.playerShirtNumber = "\(shirtNumber)"
+            self.playerPosition = position
+        } else {
+            self.playerShirtNumber = " "
+            self.playerPosition = "Not Available"
+        }
+        self.playerName = dataSource.name
+        self.shirtNumber = dataSource.shirtNumber
+    }
+    
+}
