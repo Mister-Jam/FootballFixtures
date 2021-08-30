d3d0f2f22dbe41b66eb205b4f52eac59a0d72bdd
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 11:12:57 +0100

    filter competition data to return in ascending order

--- FootballFixtures/ViewModel/CompetitionsViewModel.swift
+++ FootballFixtures/ViewModel/CompetitionsViewModel.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+
+public struct CompetitionsListViewModel {
+    let currentDate = Date()
+    let formatter = DateFormatter()
+    let calendar = Calendar.current
+    var currentYear: Int
+    var competitionName: String
+    var competitionId: Int
+    var startDate: String?
+    var endDate: String?
+    var competitions: Competitions
+    
+    init(dataSource: Competitions) {
+        competitions = dataSource
+        competitionName = competitions.name
+        competitionId = competitions.id
+        startDate = competitions.currentSeason?.startDate
+        endDate = competitions.currentSeason?.endDate
+        currentYear = calendar.component(.year, from: currentDate)
+    }
+    
+    public func displayText () -> String {
+        if let firstDate = startDate,
+           let secondDate = endDate {
+            formatter.dateFormat = "yyyy-MM-dd"
+            guard let compStartDate = formatter.date(from: firstDate) else { return "" }
+            guard let compEndDate = formatter.date(from: secondDate) else { return "" }
+            let compStartYear = calendar.component(.year, from: compStartDate)
+            let compEndYear = calendar.component(.year, from: compEndDate)
+            return "\(competitionName) \(compStartYear)/\(compEndYear%100)"
+        }
+        return ""
+    }
+}
+
+public struct FilterReturnedApiData {
+    
+    public var filteredData: [CompetitionsListViewModel]
+    
+    init(apiData: [Competitions]) {
+        let data = (apiData.map { CompetitionsListViewModel(dataSource: $0) }).filter({ data in
+            if let date = data.startDate {
+                data.formatter.dateFormat = "yyyy-MM-dd"
+                guard let compStartDate = data.formatter.date(from: date) else { return false }
+                let compStartYear = data.calendar.component(.year, from: compStartDate)
+                return data.currentYear-1 < compStartYear
+            }
+            return false
+        })
+
+        filteredData = data.sorted { $0.competitionName < $1.competitionName }
+    }
+}
