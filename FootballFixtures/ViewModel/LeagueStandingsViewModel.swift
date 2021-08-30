76b9bb063a8ffaeabff9188f411f11e689ec3924
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 11:11:03 +0100

    passed data from here to teams controller

--- FootballFixtures/ViewModel/LeagueStandingsViewModel.swift
+++ FootballFixtures/ViewModel/LeagueStandingsViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct LeagueStandingsViewModel {
+    var teamPosition: Int
+    var teamLogoUrl: String
+    var teamName: String
+    var matchesPlayed: Int
+    var goalDifference: Int
+    var totalPoints: Int
+    var teamID: Int
+    
+    init(dataSource: TeamTable) {
+        teamPosition = dataSource.position
+        teamLogoUrl = dataSource.team.crestUrl
+        teamName = dataSource.team.name
+        matchesPlayed = dataSource.playedGames
+        goalDifference = dataSource.goalDifference
+        totalPoints = dataSource.points
+        teamID = dataSource.team.id
+    }
+}
+
+
+
+
