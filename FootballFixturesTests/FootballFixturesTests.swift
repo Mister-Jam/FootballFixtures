218f066777667a241541c01d9379bf293bd3c9e2
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 17:37:55 +0100

    add few tests

--- FootballFixturesTests/FootballFixturesTests.swift
+++ FootballFixturesTests/FootballFixturesTests.swift
@@ -11,23 +11,74 @@ import XCTest
 class FootballFixturesTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_tabBarHasTwoControllers() {
+        XCTAssertEqual(HomeTabBarViewController().viewControllers?.count, 2)
+    }
+    
+    func test_FixtureControllerHasRightTitle() throws {
+        let sut = try getFirsTabController()
+        sut.loadViewIfNeeded()
+        XCTAssertNil(sut.title, "Today's Fixtures")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_controllerHasDatasourceAndDelegate() throws {
+        let sut = try getFirsTabController()
+        XCTAssertNotNil(sut.tableView.dataSource, "datasource test")
+        XCTAssertNotNil(sut.tableView.delegate, "delegate test")
+        
+        let secSut = try getSecondTabController()
+        XCTAssertNotNil(secSut.tableView.dataSource, "competitions datasource")
+        XCTAssertNotNil(secSut.tableView.delegate, "competitions delegate")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_tableStartsEmpty() throws {
+        let sut = try getSecondTabController()
+        XCTAssertTrue(sut.tableView.numberOfRows(inSection: 0) == 0)
+    }
+    
+    
+    func getFirsTabController() throws-> TodaysFixturesViewController {
+        let navigation = try XCTUnwrap(HomeTabBarViewController().viewControllers?.first as? UINavigationController)
+        let sut = try XCTUnwrap(navigation.topViewController as? TodaysFixturesViewController)
+        sut.service = MockAdapter(service: MockNetwork())
+        return sut
+    }
+    
+    func getSecondTabController() throws-> CompetitionsViewController {
+        let navigation = try XCTUnwrap(HomeTabBarViewController().viewControllers?.last as? UINavigationController)
+        let sut = try XCTUnwrap(navigation.viewControllers.last as? CompetitionsViewController)
+        sut.service = MockAdapter(service: MockNetwork())
+        return sut
     }
 
 }
+class MockNetwork: NetworkLoader {
+    func loadRequest<T>(path: String, model: T.Type, completion: @escaping ((Result<T, Error>) -> Void)) where T : Decodable {
+        let score = FullTime(homeTeam: 2, awayTeam: 2)
+        let ftScore = Score(fullTime: score)
+        let team1 = Team(id: 1, name: "Man U")
+        let team2 = Team(id: 1, name: "Away Team")
+        let firstData = Matches(utcDate: "2021-12-08", matchday: 4, score: ftScore, homeTeam: team1, awayTeam: team2)
+        let secondData = Matches(utcDate: "2021-12-08", matchday: 4, score: ftScore, homeTeam: team1, awayTeam: team2)
+        var model1 = [Matches]()
+        model1.append(firstData)
+        model1.append(secondData)
+    }
+}
+
+struct MockAdapter: NetworkLoader {
+    var service: MockNetwork
+    func loadRequest<T>(path: String, model: T.Type, completion: @escaping ((Result<T, Error>) -> Void)) where T : Decodable {
+        service.loadRequest(path: path, model: model) { result in
+            completion(result)
+        }
+    }
+}
+
