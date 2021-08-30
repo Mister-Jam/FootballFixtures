299403d7162aeafcf7207ce6e158cb23cb1078a1
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-30 11:16:36 +0100

    deleted unused properties

--- FootballFixtures/AppDelegate.swift
+++ FootballFixtures/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "TeamsData")
+        let container = NSPersistentContainer(name: "FootballData")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
